ab33852835db47c13904f39850793da1eacb2c10
Eloiza <eloiza.rossetto@gmail.com>
2021-02-05 16:25:32 -0300

    Função de pulo do dino implementada

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const dino = document.querySelector('.dino');
+const isJumping = false;
 
 function handleKeyUp(event){
 	if(event.keyCode === 32){
-		console.log("Pressionou espaco")
+		if(! isJumping){
+			jump();
+		}
 	}
 }
+
+function jump(){
+	let position = 0;
+	isJumping = true;
+
+	let upInterval = setInterval(() => {
+		//posição máxima - desce o dinossauro
+		if(position >= 150){
+			clearInterval(upInterval);
+
+			let downInterval = setInterval(() => {
+				if(position <= 0){
+					clearInterval(downInterval);
+					isJumping = false;
+				}
+				else{
+					position -= 10;
+					dino.style.bottom = position + 'px';
+				}
+			}, 15);
+
+		}
+		else{
+
+			position += 10;
+			dino.style.bottom = position + 'px';
+		}
+	
+	}, 15);
+}
+
 document.addEventListener('keyup', handleKeyUp)
